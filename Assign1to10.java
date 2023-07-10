7b8d4358fbd0f9b82139c028dd4684828ffd086c
896ma <solarcrash3@gmail.com>
2023-07-10 10:15:54 +0300

    Assign 1 to 10

---
+++ Assign1to10.java
@@ -0,0 +1,14 @@
+public class Assign1to10{
+    public static void main(String args[]){
+         int [] numbers=new int [10];
+         int j=1;
+      
+         for(int i=0;i<numbers.length;i++){
+             numbers[i]=j;
+             System.out.println(numbers[i]);
+             j++;
+             
+    }
+     
+}
+}
---
+++ Marvine.js
@@ -0,0 +1,5 @@
+
+var txt1="what a very";
+var txt2 ="nice day";
+txt1+txt2;
+
